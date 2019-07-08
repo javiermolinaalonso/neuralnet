6870404bf5ff644b6f843cc993c0f2893c8e53da
javiermolina <javier.molina@payworks.com>
2019-07-08 09:43:14 +0200

    Fixing nd4j

--- src/main/kotlin/NeuralNet.kt
+++ src/main/kotlin/NeuralNet.kt
@@ -16,7 +16,7 @@ class NeuralNet(val topology: List<Int>) {
         out.add(Pair(Nd4j.empty(), X)) //Inicializar
 
         layers.forEachIndexed { index, layer ->
-            val z: INDArray = (out[out.size - 1].second.mmul(layer.W)).sum(layer.b) //Sumas ponderadas de las neuronas
+            val z: INDArray = (out[out.size - 1].second.mmul(layer.W)).addRowVector(layer.b) //Sumas ponderadas de las neuronas
             val a: INDArray = Transforms.sigmoid(z)
             out.add(Pair(z, a))
         }
@@ -26,22 +26,22 @@ class NeuralNet(val topology: List<Int>) {
         for (l in layers.size - 1 downTo 0) {
             val a = out[l + 1].second
             if (l == layers.size - 1) {
-                deltas.add(a.sub(Y).muli(Transforms.sigmoid(a.sub(Y))))
+                deltas.add(a.sub(Y).mul(Transforms.sigmoid(a.sub(Y))))
             } else {
 
                 val derivateLayer = Transforms.sigmoidDerivative(a)
                 val matrixMult = deltas[0].mmul(layers[l+1].W.transpose())
 
-                val r = derivateLayer.muli(matrixMult)
+                val r = derivateLayer.mul(matrixMult)
                 deltas.addFirst(r)
             }
 
             //Gradient descent
-            val mean = deltas[0].transpose().muli(learningRate)//Calculamos la media de los valores de entrada (La traspuesta es pq queremos la primera columna)
+            val mean = deltas[0].transpose().mul(learningRate)//Calculamos la media de los valores de entrada (La traspuesta es pq queremos la primera columna)
             layers[l].b = layers[l].b.sub(mean)
 
             val matrix = out[l].second.transpose().mmul(deltas[0])
-            val matrixLr = matrix.muli(learningRate)
+            val matrixLr = matrix.mul(learningRate)
 
             layers[l].W = layers[l].W.sub(matrixLr)
         }
@@ -64,7 +64,7 @@ class NeuralNet(val topology: List<Int>) {
 }
 
 data class NeuralLayer(private val wSize:Int, private val numNeurons:Int) {
-    var b = Nd4j.create((0 until numNeurons).map { random() }.toDoubleArray()).reshape(numNeurons.toLong(), 1)
+    var b = Nd4j.create((0 until numNeurons).map { random() }.toDoubleArray()).reshape(1, numNeurons.toLong())
     var W = Nd4j.create((0 until wSize * numNeurons).map { random() }.toDoubleArray()).reshape(wSize.toLong(), numNeurons.toLong())
 }
 data class Neuron(var weights : List<Double>)
